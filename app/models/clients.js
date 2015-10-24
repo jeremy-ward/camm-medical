c90367472b2ffd3d58bd4cb9709029447a30b782
Jeremy Ward <jerskiward@gmail.com>
2015-10-24 16:24:34 -0600

    first draft of client model

--- app/models/clients.js
+++ app/models/clients.js
@@ -7,17 +7,45 @@ var Schema = mongoose.Schema;
 //===Set up the client schema
 var clientSchema = new Schema({
   company: String,
-  city: String,
-  state: String,
-  main_poc: {
-    name: String,
+  type: String,
+  website: String,
+  address:{
+    address: String,
+    ste: String,
+    city: String,
+    state: String,
+    zip: Number
+  },
+  mailingAddress:{
+    address: String,
+    ste: String,
+    PO_box: Number,
+    city: String,
+    state: String,
+    zip: Number
+  },
+  dateAdded: Date,
+  firstOrder: Date,
+  lastOrder: Date,
+  mainPoc: {
+    firstname: String,
+    lastname: String,
+    salutation: String,
     phone: Number,
+    alt_phone: Number,
     fax: Number,
     email: String,
-
+    alt_email: String
   },
-  other_pocs:[{
-
+  otherPocs:[{
+    firstname: String,
+    lastname: String,
+    salutation: String,
+    phone: Number,
+    alt_phone: Number,
+    fax: Number,
+    email: String,
+    alt_email: String
   }]
 });
 
