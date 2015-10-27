4a1b2f16a286a1aa0855721861292cca203854b2
Jeremy Ward <jerskiward@gmail.com>
2015-10-27 16:42:47 -0600

    pull route functions into controller file

--- server/api/customer-api.js
+++ server/api/customer-api.js
@@ -1,54 +1,28 @@
-//=== api routes for interaction with client database =====
-//=== "api/client..."
+//=== api routes for interaction with customer database =====
+//=== "api/customer..."
 
 //=== get required dependencies
-var express = require('express'),
-    Router  = express.Router();
+var express            = require('express'),
+    Router             = express.Router(),
+    customerController = require("../controllers/customer-controller");
 
 
 //=== set up api routes =========================
 
-//=== gets all clients ===================
-router.get('/find', function(req, res){
-  Client.find({active: true}, function(err, clients){
-    if(err) res.send(err);
-    res.send(clients);
-  });
-});
+  //GET Routes ========================
+    //=== gets all customer =
+router.get('/', customerController.findActive);
 
-//=== get one client by id ============
-router.get('/find/:client_id', function(req, res){
-  Client.findById(req.params.client_id
-    , function(err,client){
-      if(err) res.send(err);
-      res.send(client);
-    });
-});
+    //=== search for client =
+router.get("/search", customerController.findByTerm);
 
-//=== search for client ===============
-router.get("/search", function(req, res){
-  var term = new RegExp(req.body.search, "i");
-  Client.find({
-    $and: [
-      {active: true},
-      {$or: [
-        {company : term},
-        {website : term},
-        {address:{state : term}},
-        {address:{city : term}},
-        {mainPoc:{firstName: term}},
-        {mainPoc:{lastName: term}}
-        //add otherPocs search
-      ]}
-    ]
-  }, function(err, clients){
-    if(err) res.send(err);
-    res.send(clients);
-  });
-});
-
-//=== add a new client ================
-router.post("/new", function(req, res){
+    //=== get one client by id
+router.get('/:customer_id', customerController.findCustomer);
+  
+  
+  //POST Routes =======================
+    //=== add a new client ==
+router.post("/", function(req, res){
   Client.create(req.body.client, function(err, client){
     if(err) res.send(err);
     res.send(client);
@@ -56,7 +30,7 @@ router.post("/new", function(req, res){
 });
 
 //=== updates a client ================
-router.put("/update/:client_id", function(req, res){
+router.put("/:client_id", function(req, res){
   Client.findOneByIdAndUpdate(req.params.client_id
     , req.body.update
     , function(err, client){
--- server/controllers/customer-controller.js
+++ server/controllers/customer-controller.js
@@ -1,1 +1,28 @@
 var Customer = require('../models/customer-model.js');
+
+//===search by term provided
+module.exports.findByTerm = function(req, res){
+  Customer.findByTerm(req.body.searchTerm, 
+    function(err, customers){
+      if(err) res.send(err);
+      res.send(customers);
+    }
+  );
+};
+
+//===find all active customers
+module.exports.findActive = function(req, res){
+  Customer.findActive(function(err, customers){
+    if(err) res.send(err);
+    res.send(customers);
+  });
+};
+
+//===find one customer by ID
+module.exports.findCustomer = function(req, res){
+  Customer.findById(req.params.customer_id,
+    function(err, customer){
+      if(err) res.send(err);
+      res.send(customer);
+    });
+};
