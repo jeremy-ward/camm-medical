6f232549b4b5a8ac18703cf840f3ab542e008edd
Jeremy Ward <jerskiward@gmail.com>
2015-10-30 23:21:32 -0600

    code clean up

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
+var express = require('express'),
+    path           = require('path'),
+    favicon        = require('serve-favicon'),
+    logger         = require('morgan'),
+    cookieParser   = require('cookie-parser'),
+    bodyParser     = require('body-parser'),
+    mongoose       = require('mongoose'),
     methodOverride = require('method-override');
 
     routes       = require('./server/routes/index'),
