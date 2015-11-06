2c8caa216ac5ef59a00272d9ef62150c03c6de06
Jeremy Ward <jerskiward@gmail.com>
2015-11-05 17:50:09 -0700

    refactor to remove 2 deep callback

--- server/controllers/order-controller.js
+++ server/controllers/order-controller.js
@@ -1,19 +1,21 @@
 var Models  = require('../models/Models.js'),
     Controller = require('./db-controller'),
-    defCtrl = new Controller('Order');
+    defCtrl = new Controller('Order'),
+
+    populateOpt = [
+      {path: 'customer'},
+      {path: 'lineItems.supplier'}
+    ];
 
 module.exports = {
   findActive: function(req, res){
       if(!req.query.name){
         Models.Order.findActive(function(err, docs){
           if(err) res.send(err);
-          Models.Order.populate(docs, {path: 'customer'}, function(err, docs){
-            if(err) res.send(err);
-            Models.Order.populate(docs, {path: 'lineItems.supplier'}, function(err, docs){
-              if(err) res.send(err);
-              res.send(docs);
-            });
-          });
+          Models.Order.populate(docs, populateOpt, function(err, docs){
+                        if(err) res.send(err);
+                        res.send(docs);
+                      });
         });
       }
       else{
