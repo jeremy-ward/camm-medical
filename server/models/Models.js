1eb4a136457b0b9632164f1ad36d13817c9593d0
Jeremy Ward <jerskiward@gmail.com>
2015-11-07 06:54:02 -0700

    create files for product model in DB

--- server/models/Models.js
+++ server/models/Models.js
@@ -3,10 +3,12 @@
 //=== get the models
 var Customer = require('./customer-model.js'),
     Supplier = require('./supplier-model.js'),
-    Order    = require('./order-model.js');
+    Order    = require('./order-model.js'),
+    Product  = require('./product-model');
 
 module.exports={
-  customer: Customer,
-  supplier: Supplier,
-  Order: Order
+  customer : Customer,
+  supplier : Supplier,
+  Order    : Order,
+  Product  : Product
 };
